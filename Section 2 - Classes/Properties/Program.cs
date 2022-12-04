cf319bf65ecdf6b775353c23f293600e485ab902
jeremyoutinen <jeremyoutinen@gmail.com>
2022-12-04 13:33:20 -0600

    Properties

---
+++ Section 2 - Classes/Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new System.DateTime(1982, 1, 1));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
