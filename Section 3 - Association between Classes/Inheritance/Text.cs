1bcf4553e29cccc0c65143a03674eb9aa80c9794
jeremyoutinen <jeremyoutinen@gmail.com>
2022-12-04 20:48:28 -0600

    Inheritance

---
+++ Section 3 - Association between Classes/Inheritance/Text.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Inheritance
+{
+    partial class Program
+    {
+        public class Text : PresentationObject // In C#, a class can only have one parent
+        {
+            public int FontSize { get; set; }
+            public string FontName { get; set; }
+            public void AddHyperlink(string url)
+            {
+                Console.WriteLine("We added a link to " + url);
+            }
+        }
+    }
+}
