3dc2e8a2ff40c3663cde58f22007a8076e5e738e
jeremyoutinen <jeremyoutinen@gmail.com>
2022-12-04 20:24:20 -0600

    Exercise 1: StackOverflow Post
    Ability to create a post given a title and description and then upvote/downvote the post

---
+++ Section 2 - Classes/StackOverflow Post - Exercise 2/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackOverflow_Post___Exercise_2
+{
+    public class Post
+    {
+        public int VoteCount { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+
+        public Post()
+        {
+            CreatedDate = DateTime.Now;
+            VoteCount= 0;
+        }
+        public Post(string title, string description) : this()
+        {
+            Title = title; 
+            Description = description;
+        }
+
+        public void Upvote() { VoteCount++; }
+
+        public void Downvote() { VoteCount--; }
+    }
+}
---
+++ Section 2 - Classes/StackOverflow Post - Exercise 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post___Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var post1 = new Post("First Post", "Testing the capabilities of this new class");
+            post1.Upvote();
+            post1.Upvote();
+            Console.Write($"Post1 count after upvoting: {post1.VoteCount}\n");
+            post1.Downvote();
+            post1.Downvote();
+            post1.Downvote();
+            post1.Downvote();
+            Console.Write($"Post1 count after downvoting: {post1.VoteCount}\n");
+            Console.Write($"Post1 title: {post1.Title}\n");
+            Console.Write($"Post1 description: {post1.Description}\n");
+            Console.Write($"Post1 creation date: {post1.CreatedDate}\n");
+
+            Task.Delay(5000).Wait();
+            var post2 = new Post("Second Post", "Verifying this is a second set of data");
+            post2.Upvote();
+            post2.Upvote();
+            post2.Upvote();
+            post2.Upvote();
+            post2.Upvote();
+            post2.Upvote();
+            post2.Upvote();
+            Console.Write($"Post2 count after upvoting: {post2.VoteCount}\n");
+            Console.Write($"Post2 created date: {post2.CreatedDate}\n");
+
+        }
+    }
+}
