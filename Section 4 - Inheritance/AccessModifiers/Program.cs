f75008293b7882c3aac0c280f5af7b55eeb33052
jeremyoutinen <jeremyoutinen@gmail.com>
2022-12-05 20:48:26 -0600

    Access Modifiers

---
+++ Section 4 - Inheritance/AccessModifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon;
+using System.Collections.Generic;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            //Amazon.RateCalculator calculator= new RateCalculator(); // cannot access because the RateCalculator class is decalred as internal
+        }
+    }
+}
---
+++ Section 4 - Inheritance/Amazon/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var calculator = new RateCalculator(); // Not a good idea to instantiate this inside a method like this. This creates coupling.
+            var rating = calculator.Calculate(this);
+            Console.WriteLine("Promote logic changed");
+        }
+    }
+}
