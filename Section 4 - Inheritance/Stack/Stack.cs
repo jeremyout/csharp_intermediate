2dc821a548f173a718752795282478994e6e18f3
jeremyoutinen <jeremyoutinen@gmail.com>
2023-02-19 13:12:03 -0600

    Exercises

---
+++ Section 4 - Inheritance/Stack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private ArrayList MyStack = new ArrayList();
+        public void Push(object obj) 
+        {
+            try
+            {
+                if (obj == null) throw new InvalidOperationException("Cannot add null to the stack");
+
+                MyStack.Add(obj);
+            }
+            catch (InvalidOperationException ex)
+            {
+                // handle the InvalidOperationException
+                Console.WriteLine("An InvalidOperationException occurred: " + ex.Message);
+            }
+
+
+        }
+        public object Pop() 
+        {
+            try
+            {
+                if (MyStack.Count == 0) throw new InvalidOperationException("Stack is Empty, nothing to Pop!");
+                
+                var popped = MyStack[MyStack.Count - 1];
+                MyStack.RemoveAt(MyStack.Count - 1);
+                return popped;
+            }
+            catch (InvalidOperationException ex) 
+            {
+                // handle the InvalidOperationException
+                Console.WriteLine("An InvalidOperationException occurred: " + ex.Message);
+                return null;
+            }
+
+            
+
+            
+        }
+        public void Clear() 
+        {
+            MyStack.Clear();
+        }
+    }
+}
