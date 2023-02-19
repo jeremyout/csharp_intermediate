26dc1ac7bc16a6bef2c28ad8641938c02cbd97dd
jeremyoutinen <jeremyoutinen@gmail.com>
2023-02-19 14:11:55 -0600

    Method Overriding
    Position class wasn't touched on but should be fine without it for this demo

---
+++ Section 5 - Polymorphism/MethodOverriding/MethodOverriding/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+            shapes.Add(new Triangle());
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+        }
+    }
+}
